84792d87dc4ed629a799ba41ed203ff919e7c0c8
Mihai Hiticas <hiticasmihai7@gmail.com>
2026-01-28 20:45:46 +0200

    add login and signup

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,30 @@
 import { Link } from "react-router-dom";
+import { useLogout } from "../../hooks/useLogout";
+import { useAuthContext } from "../../hooks/useAuthContext";
 import './Navbar.scss';
 
 const Navbar = () => {
+  const { logout } = useLogout();
+  const { user } = useAuthContext();
+
+  const handleClick = () => {
+    logout();
+  }
+
   return (
     <header className="navbar">
-        <Link to="/">Home</Link>
-        <Link to="/workout">Workouts</Link>
-        <Link to="/about">About</Link>
-        <Link to="/contact">Contact</Link>
+        {user && <div>
+          <span>{user.email}</span>
+          <Link to="/">Home</Link>
+          <Link to="/workout">Workouts</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+          <button onClick={handleClick}>Logout</button>
+        </div>}
+        {!user && <div>
+          <Link to="/login">Login</Link>
+          <Link to="/signup">Sign Up</Link>
+        </div>}
     </header>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,9 @@ import About from './pages/About/About.jsx';
 import Contact from './pages/Contact/Contact.jsx';
 import Workout from './pages/Workout/Workout.jsx';
 import { WorkoutsContextProvider } from './context/WorkoutContext.jsx';
+import { AuthContextProvider } from './context/AuthContext.jsx';
+import Login from './pages/Login/Login.jsx';
+import Signup from './pages/Signup/Signup.jsx';
 import './index.scss';
 
 const router = createBrowserRouter([
@@ -28,12 +31,22 @@ const router = createBrowserRouter([
     path: '/contact',
     element: <Contact />,
   },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/signup',
+    element: <Signup />,
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <WorkoutsContextProvider>
-      <RouterProvider router={router} />
-    </WorkoutsContextProvider>
+    <AuthContextProvider>
+      <WorkoutsContextProvider>
+        <RouterProvider router={router} />
+      </WorkoutsContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
